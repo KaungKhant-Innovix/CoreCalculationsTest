9db2e4e319972946de1702804f8247df9bb98dff
KaungKhantInnovix <kaungkhant@innovix-solutions.com>
2025-02-28 13:54:16 +0630

    Removed bin and obj from change tracker

--- TestCodes/obj/Debug/net8.0/TestCodes.AssemblyInfo.cs
+++ TestCodes/obj/Debug/net8.0/TestCodes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d402291dbe2aff84520e1722604141a7f0c4f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3e52b82ecd66825d0aab8e3f09b28ee9cfcc65")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestCodes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
